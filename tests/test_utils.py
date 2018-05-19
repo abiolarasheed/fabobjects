2adf87949df0b207c2363a272ecc6dfa140ec18c
abi <rasheed.abiola3@gmail.com>
2018-05-19 20:39:40 +0100

    Created test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,25 @@
+# !*-* coding:utf-8 *-*
+from __future__ import unicode_literals
+
+import unittest
+
+
+class UtilsTestCase(unittest.TestCase):
+    def random_password(self):
+        pass
+
+    def return_distinct_servers(self):
+        pass
+
+    def _print(self):
+        pass
+
+    def timing(self):
+        pass
+
+    def log_call(self):
+        pass
+
+
+class ServerHostManagerTestCase(unittest.TestCase):
+    pass
