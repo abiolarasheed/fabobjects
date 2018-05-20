cb442d76b844faf1cc809f989964e2aa7c4ea9ee
abi <rasheed.abiola3@gmail.com>
2018-05-20 13:49:06 +0100

    Created settings.py

---
+++ fabobjects/settings.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from os.path import expanduser
+from os.path import join
+from fabric.api import env
+
+
+HOME = expanduser("~")
+env.user = ''
+env.server_name = ''
+env.colors = True
+env.format = True
+
+env.hosts = []
+env.passwords = {}
+env.key_filename = join(HOME, '.ssh/id_rsa.pub')
+env_settings = env
