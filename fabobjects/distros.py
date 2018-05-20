41a0af9f902a002a70385bf33db39cfeac676704
abi <rasheed.abiola3@gmail.com>
2018-05-20 13:33:08 +0100

    Implemented BaseServer

--- fabobjects/distros.py
+++ fabobjects/distros.py
@@ -1,2 +1,18 @@
 # coding: utf-8
 from __future__ import with_statement
+
+
+class BaseServer(object):
+    """
+    An SSH daemon connection.
+    **Basics**
+    This class performs useful high level operations over ssh.
+    """
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def sudo(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def run(self, *args, **kwargs):
+        raise NotImplementedError
