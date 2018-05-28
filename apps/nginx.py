5a419a86c1a78b51989f2ce1e9f92876c5233132
abi <rasheed.abiola3@gmail.com>
2018-05-28 12:51:34 +0100

    Created NginxServer

---
+++ apps/nginx.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+
+from apps.base import BaseApp
+
+
+class NginxServer(BaseApp):
+    def __init__(self, *args, **kwargs):
+        super(NginxServer, self).__init__(*args, **kwargs)
+        self.service_name = 'nginx'
+        self.service_port = kwargs.get('service_port', '80')
