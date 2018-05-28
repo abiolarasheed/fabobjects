07406ecfd69ba3feea808acce091ec3fb5ed5621
abi <rasheed.abiola3@gmail.com>
2018-05-28 12:50:04 +0100

    Created DjangoApp

---
+++ apps/django.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from apps.base import BaseApp
+
+
+class DjangoApp(BaseApp):
+    def __init__(self, *args, **kwargs):
+        super(DjangoApp, self).__init__(*args, **kwargs)
+        self.app_user = kwargs.get('app_user') or self.env.app_user
+        self.supervised = kwargs["supervised"]
