8e62cd3d440716bcef330365b581cafc9802f487
abi <rasheed.abiola3@gmail.com>
2018-06-25 15:35:38 +0100

    Updated PostgresServer

--- apps/postgres.py
+++ apps/postgres.py
@@ -4,12 +4,16 @@ from __future__ import unicode_literals
 import getpass
 import os
 import sys
+
+from fabric.api import hide
+from fabric.api import settings
+
 from apps.base import BaseApp
 from fabobjects.utils import server_host_manager, random_password
 
 
-PG_VERSION = "9.6"
-GIS_VERSION = "2.3"
+PG_VERSION = "9.5"
+GIS_VERSION = "2.2"
 HBA_TEXT = ('local   all    postgres                     ident\n'
             'host    replication replicator  0.0.0.0/0   md5\n'
             'local   all    all                          password\n'
@@ -37,7 +41,9 @@ class PostgresServer(BaseApp):
         super(PostgresServer, self).__init__(*args, **kwargs)
         self.name = 'master_setup'
         self.service_name = 'postgresql'
-        self.db_pass = kwargs.get("db_pass", None)
+        self.db_pass = kwargs.get("db_pass", None) or self.get_db_pass()
+        self.db_name = kwargs.get("db_name", None)
+        self.db_user = kwargs.get("db_user", None)
         self.replicator_pass = kwargs.get('replicator_pass', None)
         self.service_port = kwargs.get('service_port', '5432')
         self.db_version = kwargs.get("db_version", PG_VERSION)
@@ -50,48 +56,85 @@ class PostgresServer(BaseApp):
         self.version_directory_join = kwargs.get("version_directory_join", '.')
 
     @server_host_manager
-    def __get_db_version(self, db_version=PG_VERSION):
-        if not db_version:
-            db_version = self.db_version
-        return self.version_directory_join.join(db_version.split('.')[:2])
-
-    @server_host_manager
-    def __install(self, enable_postgis=False, db_version=PG_VERSION,
-                  gis_version=GIS_VERSION):
+    def deploy(self, db_version=PG_VERSION, encrypt='md5', gis_version=GIS_VERSION,
+               enable_postgis=False, master=False, db_name=None, db_user=None,
+               passwd=None):
         """
-
-        :param enable_postgis:
-        :param db_version:
-        :param gis_version:
+        Install and configure postgres.
+        :param db_version: Postgres Version to install
+        :param string encrypt: Set encryption type for user password.
+        :param string gis_version: PostGis Version to install
+        :param bool enable_postgis: Configure PostGis if true
+        :param bool master: Set True if it's a master db of a cluster
+        :param string db_name: Name of Data base you are creating
+        :param db_user: Name of user for the db you are creating
+        :param string passwd: Password for the user you are creating
         :return:
         """
         db_version = db_version or self.db_version
         gis_version = gis_version or self.gis_version
-        self.install_package("postgresql-{0}-postgis-{1}"
-                             .format(db_version, gis_version))
 
-        if enable_postgis:
-            self.enable_postgis()
+        db_name = db_name or self.db_name
+        db_user = db_user or self.db_user
+        passwd = passwd or self.db_pass
 
-        self.install_package("pgtune")
+        if all([db_version, gis_version]):
+            package = "postgresql-{0}-postgis-{1}".format(db_version, gis_version)
 
-    def install(self):
-        return self.__install()
+        else:
+            package = "postgresql-{0}".format(db_version)
+
+        try:
+            self.install_package(package)
+            if enable_postgis:
+                try:
+                    self.create_postgis_db(db=db_name)
+                except:
+                    pass
+
+            self.create_db(dbname=db_name)
+            self.create_db_user(user=db_user, passwd=passwd)
+            self.grant_permision(permission_type='All',
+                                 db=db_name, role_name=db_user)
+
+            data_dir = self.__get_data_dir(db_version)
+            config_dir = self.__get_config_dir(db_version, )
+            config = dict(self.postgres_config)
+            config['archive_command'] = "'cp {0} {1}/wal_archive/{2}'".format('%p', data_dir, '%f')
+            self.__setup_hba_config(config_dir, encrypt)
+            self.__setup_postgres_config(config_dir, config)
+            self.__setup_archive_dir(data_dir)
+            self.service_restart()
+
+            if master:
+                self.__create_replicator()
+        except:
+            return
+
+    @server_host_manager
+    def __get_db_version(self, db_version=None):
+        if not db_version or db_version is None:
+            db_version = self.db_version
+        return self.version_directory_join.join(db_version.split('.')[:2])
 
     @server_host_manager
     def turn_pg(self):
-        config_file = '/etc/postgresql/{0}/main/postgresql.conf'.format(self.db_version)
-        out_file = 'postgresql.conf'
+        self.install_package("pgtune")
+        if self.is_package_installed("pgtune"):
+            config_file = '/etc/postgresql/{0}/main/postgresql.conf'.format(self.db_version)
+            out_file = 'postgresql.conf'
 
-        self.sudo('python pgtune -i {0} -o {1}'.format(config_file, out_file))
-        self.sudo('cp {0} {1}.bak'.format(config_file, config_file))
-        self.sudo('mv {0} {1}'.format(out_file, config_file))
-        self.service_reload('postgresql')
+            self.sudo('python pgtune -i {0} -o {1}'.format(config_file, out_file))
+            self.sudo('cp {0} {1}.bak'.format(config_file, config_file))
+            self.sudo('mv {0} {1}'.format(out_file, config_file))
+            self.service_reload('postgresql')
+        else:
+            print("E: Unable to locate package pgtune")
 
     @server_host_manager
     def psql(self, sql, show=True, use_table=None):
         """ Runs SQL against the project's database. """
-        if use_table:
+        if use_table and use_table is not None:
             psql_ = 'psql {0} -c'.format(use_table)
         else:
             psql_ = 'psql -c'
@@ -108,33 +151,32 @@ class PostgresServer(BaseApp):
         self.psql(command)
 
     @server_host_manager
-    def create_user(self, user=None, passwd=None):
-        with self.settings(warn_only=True):
+    def create_db_user(self, user=None, passwd=None):
+        with settings(warn_only=True):
             command = "CREATE USER {0}".format(user)
             self.psql(command)
 
             if passwd:
-                with self.hide('running', 'stdout', 'stderr'):
-                    command = 'ALTER USER {0} WITH ENCRYPTED ''PASSWORD \"\'{1}\'\"'.format(user, passwd)
+                with hide('running', 'stdout', 'stderr'):
+                    command = 'ALTER USER {0} WITH ENCRYPTED ''PASSWORD \"\'{1}\'\";'.format(user, passwd)
                     self.psql(command)
 
     @server_host_manager
-    def grant_permision(self, permission_type='All', table_name=None, role_name=None):
-        opts = dict(permission_type=permission_type, table_name=table_name, role_name=role_name)
-        command = "GRANT %(permission_type)s ON DATABASE %(table_name)s TO %(role_name)s".format(**opts)
+    def grant_permision(self, permission_type='All', db=None, role_name=None):
+        opts = dict(permission_type=permission_type, db=db, role_name=role_name)
+        command = "GRANT {permission_type} ON DATABASE {db} TO {role_name}".format(**opts)
         self.psql(command)
 
     @server_host_manager
-    def create_postgis_db(self, db):
-        self.create_db(dbname=db)
-        self.psql('CREATE EXTENSION postgis;', use_table=db)
-        self.psql('CREATE EXTENSION postgis_topology;', use_table=db)
-
-    @server_host_manager
-    def enable_postgis(self):
-        with self.settings(warn_only=True):
-            self.psql("CREATE EXTENSION postgis;")
-            self.psql("CREATE EXTENSION postgis_topology;")
+    def create_postgis_db(self, db=None):
+        with settings(warn_only=True):
+            if db is not None:
+                self.create_db(dbname=db)
+                self.psql('CREATE EXTENSION postgis;', use_table=db)
+                self.psql('CREATE EXTENSION postgis_topology;', use_table=db)
+            else:
+                self.psql("CREATE EXTENSION postgis;")
+                self.psql("CREATE EXTENSION postgis_topology;")
 
     def __get_data_dir(self, db_version):
         data_dir = '/var/lib/postgresql/{0}/main'.format(db_version)
@@ -152,14 +194,15 @@ class PostgresServer(BaseApp):
     def __setup_parameter(self, filename, **kwargs):
         for key, value in kwargs.items():
             origin = "#{0} =".format(key)
-            new = "{0} = {1}" %(key, value)
+            new = "{0} = {1}".format(key, value)
             self.sudo('sed -i "/{0}/ c\{1}" {2}'.format(origin, new, filename))
 
     @server_host_manager
     def __setup_hba_config(self, config_dir, encrypt=None):
         """ enable postgres access without password from localhost """
-        if not encrypt:
+        if not encrypt or encrypt is not None:
             encrypt = self.encrypt
+
         hba_conf = os.path.join(config_dir, 'pg_hba.conf')
         kwargs = {'encrypt': encrypt}
         hba_text = self.hba_text % kwargs
@@ -194,12 +237,12 @@ class PostgresServer(BaseApp):
         return archive_dir
 
     @server_host_manager
-    def __get_home_dir(self):
-        return self.__get_home_dir(user='postgres')[0]
+    def get_home_dir(self):
+        return self._get_home_dir(user='postgres')[0]
 
     @server_host_manager
     def __setup_ssh_key(self, pswd):
-        ssh_dir = os.path.join(self.__get_home_dir(), '.ssh')
+        ssh_dir = os.path.join(self.get_home_dir(), '.ssh')
         self.sudo('mkdir -p {0}'.format(ssh_dir))
         rsa = os.path.join(ssh_dir, 'id_rsa')
 
@@ -219,13 +262,14 @@ class PostgresServer(BaseApp):
         self.psql(command)
 
     @server_host_manager
-    def __create_replicator(self, replicator_pass=None):#db_version, section):
+    def __create_replicator(self, replicator_pass=None):
         if replicator_pass is None and self.replicator_pass is None:
-            self.replicator_pass = random_password(12)
+            self.replicator_pass = replicator_pass = random_password(12)
             command = 'CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD \"\'{0}\'\"'.format(replicator_pass)
+
             self.psql(command)
 
-            history_file = os.path.join(self.__get_home_dir(), '.psql_history')
+            history_file = os.path.join(self.get_home_dir(), '.psql_history')
 
             if self.exists(history_file):
                 self.sudo('rm {0}'.format(history_file))
@@ -235,49 +279,7 @@ class PostgresServer(BaseApp):
             return None
 
     @server_host_manager
-    def _run(self, db_version=PG_VERSION, encrypt=None, save_config=False,
-             enable_postgis=False, master=False, dbname=None, user=None,
-             passwd=None, **kwargs):
-        """
-
-        :param db_version:
-        :param encrypt:
-        :param save_config:
-        :param enable_postgis:
-        :param master:
-        :param dbname:
-        :param user:
-        :param passwd:
-        :param kwargs:
-        :return:
-        """
-        self.__install(enable_postgis=enable_postgis, db_version=db_version,
-                       gis_version=kwargs.get('gis_version'))
-
-        data_dir = self.__get_data_dir(db_version)
-        config_dir = self.__get_config_dir(db_version,)
-        config = dict(self.postgres_config)
-        config['archive_command'] = ("'cp %s %s/wal_archive/%s'" %('%p', data_dir, '%f'))
-        self.__setup_hba_config(config_dir, encrypt)
-        self.__setup_postgres_config(config_dir, config)
-        self.__setup_archive_dir(data_dir)
-        self.service_restart()
-
-        if enable_postgis:
-            self.create_postgis_db(dbname)
-        else:
-            self.create_db(dbname=dbname)
-        self.create_user(user=user, passwd=passwd)
-        self.grant_permision(permission_type='All',
-                             table_name=dbname, role_name=user)
-        if master:
-            self.__create_replicator()
-
-        if save_config:
-           self.env.config_object.save(self.env.conf_filename)
-
-    @server_host_manager
-    def db_pass(self):
+    def get_db_pass(self):
         """ Prompts for the database password if unknown. """
         if not self.db_pass or self.db_pass is None:
             self.db_pass = getpass("Enter the database password: ")
@@ -297,17 +299,17 @@ class PostgresServer(BaseApp):
     @server_host_manager
     def backup(self, project_name, filename):
         """ Backs up the database. """
-        return self.postgres_run("pg_dump -Fc {0} > {0}".format(project_name, filename))
+        return self.postgres_run("pg_dump -Fc {0} > {1}".format(project_name, filename))
 
     @server_host_manager
     def restore(self, project_name, filename):
         """ Restores the database. """
-        return self.postgres_run("pg_restore -c -d %s %s" % (project_name, filename))
+        return self.postgres_run("pg_restore -c -d {0} {1}".format(project_name, filename))
 
     @server_host_manager
     def set_daily_backup(self, password):
         # configure daily dumps of all databases
-        self.dir_ensure('/var/backups/postgresql', recursive=True)
+        self.sudo('mkdir -p /var/backups/postgresql')
         self.echo("localhost:*:*:postgres:{0}".format(password),
                   to="/root/.pgpass", use_sudo=True, append=True)
         self.sudo('chmod 600 /root/.pgpass')
